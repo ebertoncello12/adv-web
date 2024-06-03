ab5060616151aec12ef39e31324f841176f912ee
enzzoferrari <enzzoferrariberto@gmail.com>
2024-06-03 11:33:19 -0300

    feat: add adv

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import AlertReducer from './messages/AlertReducer';
 import UserReducer from './user/UserReducer';
-import LoadingReducer from './loading/LoadingReducer';
 
 export default configureStore({
   reducer: {
-    loading: LoadingReducer,
     user: UserReducer,
-    alert: AlertReducer,
   },
 });
